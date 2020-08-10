ca680081cebd3f5e5dc03743accc0f18c110abc0
Caio Silva <bladellano@gmail.com>
2020-08-10 15:27:23 -0300

    Listando todas as paginas

--- principal.php
+++ principal.php
@@ -3,22 +3,35 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Principal</title>
-	<link rel="stylesheet" href="">
+	<title>Baggio - Páginas construídas</title>
+
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<style>
+		body{
+			background-color: var(--cor-primaria);
+			margin-top: 20px;
+		}
+	</style>
 </head>
 <body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1 class="display-4">Link das páginas construídas</h1>
+			<p class="lead">Basta clicar no link que irá abrir uma nova aba</p>
 
-	<ul>
-		<li><a target="_blank" href="vendas.php">vendas</a></li>
-		<li><a target="_blank" href="pesquisa3.php">pesquisa3</a></li>
-		<li><a target="_blank" href="perfil-imovel.php">perfil-imovel</a></li>
-		<li><a target="_blank" href="locacoes.php">locacoes</a></li>
-		<li><a target="_blank" href="index.php">index</a></li>
-		<li><a target="_blank" href="home2.php">home2</a></li>
-		<li><a target="_blank" href="contato.php">contato</a></li>
-		<li><a target="_blank" href="__footer.php">__footer</a></li>
-		<li><a target="_blank" href="__header.php">__header</a></li>
-	</ul>
-	
+			<ul class="list-group">
+				<li class="list-group-item"><a target="_blank" href="vendas.php">vendas</a></li>
+				<li class="list-group-item"><a target="_blank" href="pesquisa3.php">pesquisa3</a></li>
+				<li class="list-group-item"><a target="_blank" href="perfil-imovel.php">perfil-imovel</a></li>
+				<li class="list-group-item"><a target="_blank" href="locacoes.php">locacoes</a></li>
+				<li class="list-group-item"><a target="_blank" href="index.php">index</a></li>
+				<li class="list-group-item"><a target="_blank" href="home2.php">home2</a></li>
+				<li class="list-group-item"><a target="_blank" href="contato.php">contato</a></li>
+				<li class="list-group-item"><a target="_blank" href="__footer.php">__footer</a></li>
+				<li class="list-group-item"><a target="_blank" href="__header.php">__header</a></li>
+			</ul>
+		</div>
+	</div>
 </body>
 </html>
