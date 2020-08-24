13d415c1628f9b194fe6394837fd8253e5305e9a
Caio Silva <bladellano@gmail.com>
2020-08-24 07:01:38 -0300

    Organização dos arquivos para apresentação

---
+++ home.php
@@ -0,0 +1,170 @@
+<?php require("__header.php"); ?>
+
+<ol class="breadcrumb">
+	<li class="breadcrumb-item"><a href="#">Home</a></li>
+	<li class="breadcrumb-item active" aria-current="page">A Baggio</li>
+</ol>
+
+<div class="container-fluid main-baggio">
+
+	<div class="section">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="img-fluid" src="assets/images/baggio-imoveis-fachada.jpg" alt="Fachada">
+			</div>
+			<div class="col-md-8">
+				<h3><span>A BAGGIO</span></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet.</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- NOSSA HISTÓRIA -->
+	<div class="section">
+		<div class="row">				
+			<div class="col-md-12">
+				<h3><span>NOSSA HISTÓRIA</span></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat quas odio dolores libero molestias quasi, ad autem nostrum voluptate itaque modi doloremque eligendi vel eum aut cupiditate, provident error eveniet.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+
+				<ul class="cards-baggio">
+					<li>
+						<a href="#" class="card">
+							<i class="fas fa-bullseye"></i>
+							<h4>Missão</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="card">
+							<i class="fas fa-eye"></i>
+							<h4>Visão</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="card">
+							<i class="fas fa-hands"></i>
+							<h4>Valores</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+						</a>
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- QUEM SOMOS -->
+	<div class="section">
+		<h3><span>QUEM SOMOS</span></h3>
+		<div class="row">				
+			<div class="col-md-12">
+				<ul class="accountants">
+					<li><div><h4>3500</h4><p>Total de imóveis vendidos/alugados</p></div></li>
+					<li><div><h4>3500</h4><p>Imóveis para Alugar</p></div></li>
+					<li><div><h4>3500</h4><p>Imóveis para Vender</p></div></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<!-- PARCEIROS -->
+
+	<div class="section">
+		<h3><span>QUEM SOMOS</span></h3>				
+
+		<div class="row">				
+			<div class="col-md-12">
+				<div class="wrap-peoples">
+					<div>
+						<img src="assets/images/perfil-depoimento.jpg">
+						<h4>Flávia Nobrega</h4>
+						<p>Designer</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+
+					<div>
+						<img src="assets/images/perfil-depoimento-mulher-a.jpg">
+						<h4>Sandra Nobrega</h4>
+						<p>Product Owner</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+
+					<div>
+						<img src="assets/images/perfil-depoimento-a.jpg">
+						<h4>Afonso Silva</h4>
+						<p>Designer</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+
+					<div>
+						<img src="assets/images/perfil-depoimento-mulher.jpg">
+						<h4>Gabriel Nobrega</h4>
+						<p>Product Owner</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+
+					<div>
+						<img src="assets/images/perfil-depoimento-b.jpg">
+						<h4>Felipe Nobrega</h4>
+						<p>Designer</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+
+					<div>
+						<img src="assets/images/perfil-depoimento-c.jpg">
+						<h4>Fábio Cereja</h4>
+						<p>Product Owner</p>
+						<p>Responsável pelo andamento dos projetos da agência</p>
+					</div>
+				</div><!-- wrap-peoples -->
+			</div>				
+		</div>
+
+	</div>
+
+	<!-- PARCEIROS -->
+	<div class="section">
+		<h3><span>PARCEIROS</span></h3>
+		<div class="row">
+			<div class="col-xs-12 offset-md-1 col-md-10">
+				<div class="partners-slick">						
+					<div><img src="assets/images/logo_3170210.png"></div>
+					<div><img src="assets/images/logo_970530.png"></div>
+					<div><img src="assets/images/denver-broncos-logo-1536x1226.png"></div>
+					<div><img src="assets/images/banco-bv-logo-1536x1084.png"></div>
+					<div><img src="assets/images/duracell-logo-1536x512.png"></div>
+					<div><img src="assets/images/kabum-logo.png"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- CLIENTES -->
+	<div class="section">
+		<h3><span>CLIENTES</span></h3>
+		<div class="row">
+			<div class="col-xs-12 offset-md-1 col-md-10">
+				<div class="partners-slick">						
+					<div><img src="assets/images/logo_970530.png"></div>
+					<div><img src="assets/images/logo_3170210.png"></div>
+					<div><img src="assets/images/logo_970530.png"></div>
+					<div><img src="assets/images/logo_3170210.png"></div>
+					<div><img src="assets/images/logo_970530.png"></div>
+					<div><img src="assets/images/logo_3170210.png"></div>						
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div> <!-- CONTAINER-FLUID -->
+
+<?php require("__footer.php") ?>
